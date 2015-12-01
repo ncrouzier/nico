01b82fff3242a23218e485cd80a67e85df4b83a9
Nicolas Crouzier <nicolas.crouzier@nist.gov>
2015-12-01 14:30:26 -0500

    sdf

---
+++ src/main/java/nico/test/nico/CopyOfApp.java
@@ -0,0 +1,13 @@
+package nico.test.nico;
+
+/**
+ * Hello world!
+ *
+ */
+public class CopyOfApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello asdasd!1" );
+    }
+}
